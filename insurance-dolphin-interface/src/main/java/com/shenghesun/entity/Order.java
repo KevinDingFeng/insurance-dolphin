cec74f275fe212406d56391fa408916b46762795
zdd382867915 <382867915@qq.com>
2018-09-27 10:34:58 +0800

    a

---
+++ insurance-dolphin-interface/src/main/java/com/shenghesun/entity/Order.java
@@ -0,0 +1,14 @@
+package com.shenghesun.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+//订单
+@Entity
+@Table
+@Data
+public class Order {
+
+}
