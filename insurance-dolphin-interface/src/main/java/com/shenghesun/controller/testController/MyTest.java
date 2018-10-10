90cabcdf3a426533c36ca385ef96b6ab0356bbed
zdd382867915 <382867915@qq.com>
2018-10-10 18:56:03 +0800

    update

--- insurance-dolphin-interface/src/main/java/com/shenghesun/controller/testController/MyTest.java
+++ insurance-dolphin-interface/src/main/java/com/shenghesun/controller/testController/MyTest.java
@@ -8,7 +8,7 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		String resultStr = "{\"output\":{\"result\":[{\"flightNo\":\"KN5987\",\"rate\":\"59.33\",\"depCity\":\"北京南苑\",\"depCode\":\"NAY\",\"arrCity\":\"上海浦东\",\"arrCode\":\"PVG\",\"depPort\":\"北京南苑机场\",\"arrPort\":\"上海浦东国际机场\",\"depTerminal\":\"\",\"arrTerminal\":\"T1\",\"depScheduled\":\"2018-09-30T20:50:00Z\",\"arrScheduled\":\"2018-09-30T23:15:00Z\",\"depEstimated\":\"0001-01-01T00:00:00Z\",\"arrEstimated\":\"0001-01-01T00:00:00Z\",\"depActual\":\"0001-01-01T00:00:00Z\",\"arrActual\":\"0001-01-01T00:00:00Z\",\"codeShares\":[]}]}}";
-		JsonParser jp = new JsonParser();
+		JsonParser jp = new JsonParser();  
 		JsonObject jo = jp.parse(resultStr).getAsJsonObject();
 		JsonArray resultJsonArray = jo.get("output").getAsJsonObject().get("result").getAsJsonArray();
 		JsonObject result = jp.parse(resultJsonArray.get(0).toString()).getAsJsonObject();
