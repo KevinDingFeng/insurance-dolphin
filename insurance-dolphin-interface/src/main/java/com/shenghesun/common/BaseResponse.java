68cf1786454c30c9f5dfec1dd61a41070ce36423
zdd382867915 <382867915@qq.com>
2018-10-24 11:27:56 +0800

    update

--- insurance-dolphin-interface/src/main/java/com/shenghesun/common/BaseResponse.java
+++ insurance-dolphin-interface/src/main/java/com/shenghesun/common/BaseResponse.java
@@ -12,9 +12,9 @@ public class BaseResponse implements Serializable {
 	public static final int user_check_failed_code = 102;
 	public static final long ex = 60 * 60 * 24 * 30;//token 过期时间 单位：秒
 	public static final long redis_ex = 60 * 30;//redis 过期时间 单位：秒
-	public static final String inner_price = "15";//国内行李保险价格
+	public static final String inner_price = "10";//国内行李保险价格
 	public static final String inner_type = "1";//国内类型
-	public static final String foreign_price = "28";//国外行李保险价格
+	public static final String foreign_price = "20";//国外行李保险价格
 	public static final String foreign_type = "2";//国外类型
 	public static final String error_price = "00";//错误代码，用于前端区分
 	public static final String error_price1 = "01";//错误代码，用于前端区分
