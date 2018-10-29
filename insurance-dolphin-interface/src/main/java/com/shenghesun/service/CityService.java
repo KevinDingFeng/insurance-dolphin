1c391daa17650a63273b238c9b99ee00c941346f
yangzp <yangzp@shenghesun.com>
2018-10-29 09:53:03 +0800

    update

--- insurance-dolphin-interface/src/main/java/com/shenghesun/service/CityService.java
+++ insurance-dolphin-interface/src/main/java/com/shenghesun/service/CityService.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -21,7 +19,7 @@ import com.shenghesun.util.RedisUtil;
 
 @Service
 public class CityService {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	//private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private CityDao cityDao;
 	@Autowired
