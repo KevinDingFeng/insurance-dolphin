375dc78d99af2ff2978761f771a3ad3ebf99b9a9
yangzp <yangzp@shenghesun.com>
2018-10-29 13:24:46 +0800

    updae

--- insurance-dolphin-interface/src/main/java/com/shenghesun/entity/City.java
+++ insurance-dolphin-interface/src/main/java/com/shenghesun/entity/City.java
@@ -1,14 +1,9 @@
 package com.shenghesun.entity;
 
-import java.io.Serializable;
-
-import javax.persistence.Entity;
-
 import lombok.Data;
 //城市起止信息
-@Entity
 @Data
-public class City extends BaseEntity implements Serializable{
+public class City {
 	
 	private String depCity;
 	
