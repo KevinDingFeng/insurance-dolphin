59a7c71cc83058493b495218126277dacdcdb8a1
yangzp <yangzp@shenghesun.com>
2018-11-13 14:28:34 +0800

    update

--- insurance-dolphin-interface/src/main/java/com/shenghesun/util/SmsCodeService.java
+++ insurance-dolphin-interface/src/main/java/com/shenghesun/util/SmsCodeService.java
@@ -17,6 +17,7 @@ import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
+import com.shenghesun.util.cpic.StringUtils;
 
 
 /**
@@ -39,22 +40,19 @@ public class SmsCodeService {
 	@Value("${sms.app.secret}")
 	private String smsAppSecret;
 	
-	@Value("${sms.template.code}")
-	private String templateCode;
-
-
 	/**
-	 * 发送验证码到手机号
-	 * 
-	 * @author hao.tan
-	 * @param number
-	 *            手机号码
-	 * @param code
-	 *            验证码
-	 * @return
-	 * @throws ClientException
-	 */
-	public String sendSmsCode(String number, String code) {
+	 * 发送短信
+	 * @Title: sendSms 
+	 * @Description: TODO 
+	 * @param phoneNumbers 手机号
+	 * @param signName 短信签名
+	 * @param templateCode 短信模板code
+	 * @param templateParam 模板中的变量替换JSON串
+	 * @return  String 
+	 * @author yangzp
+	 * @date 2018年11月2日上午10:27:41
+	 **/ 
+	public String sendSms(String phoneNumbers, String signName, String templateCode, String templateParam) {
 		// 设置超时时间-可自行调整
 		System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
 		System.setProperty("sun.net.client.defaultReadTimeout", "10000");
@@ -78,16 +76,17 @@ public class SmsCodeService {
 			// 使用post提交
 			request.setMethod(MethodType.POST);
 			// 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
-			request.setPhoneNumbers(number);
+			request.setPhoneNumbers(phoneNumbers);
 			// 必填:短信签名-可在短信控制台中找到
-//			request.setSignName("大宗禾秱");
-			request.setSignName("盛禾阳光 ");
+			request.setSignName(signName);
 			// 必填:短信模板-可在短信控制台中找到
 			request.setTemplateCode(templateCode);
 			// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
 			// 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-			//request.setTemplateParam("{no}");
-			request.setTemplateParam("{\"no\":\"" + code + "\"}");
+			if(StringUtils.isNotEmpty(templateParam)) {
+				request.setTemplateParam(templateParam);
+			}
+			
 			// 可选-上行短信扩展码(无特殊需求用户请忽略此字段)
 			// request.setSmsUpExtendCode("90997");
 			// 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
@@ -98,10 +97,10 @@ public class SmsCodeService {
 			if(sendSmsResponse != null && "OK".equals(sendSmsResponse.getCode())){
 				return "success";
 			}
-			log.info(sendSmsResponse.toString());
+			log.info(sendSmsResponse.getCode()+":"+sendSmsResponse.getMessage());
 			return "failed";
 		} catch (ClientException e) {
-			log.error("Exception {} in {} ", e.getMessage(), "sendSmsCode:"+number);
+			log.error("Exception {} in {} ", e.getMessage(), "sendSmsCode:"+phoneNumbers);
 			return "failed";
 		}
 	}
