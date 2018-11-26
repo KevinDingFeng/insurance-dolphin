e9c728135af3ed4b6201206167c8ab5b522a533b
zdd382867915 <382867915@qq.com>
2018-11-26 18:55:20 +0800

    update

--- insurance-dolphin-interface/src/main/java/com/shenghesun/controller/pay/WxpayNotifyController.java
+++ insurance-dolphin-interface/src/main/java/com/shenghesun/controller/pay/WxpayNotifyController.java
@@ -35,24 +35,24 @@ import com.shenghesun.util.wxpay.WXPayUtil;
 @RestController
 @RequestMapping(value = "/wxpay")
 public class WxpayNotifyController {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private PayService payService;
-	
+
 	@Autowired
 	private AsyncService asyncService;
-	
+
 	@Autowired
 	private SmsCodeService smsCodeService;
-	
+
 	@Autowired
 	private WxPayResultService WxPayService;
-	
+
 	@Value("${weixin.notify.url}")
 	private String notifyUrl;
-	
+
 	@Autowired
 	private WXPayConfig conf;
 	/**
@@ -60,7 +60,7 @@ public class WxpayNotifyController {
 	 */
 	@Value("${sms.success.template.code}")
 	private String templateCode;
-	
+
 	public String getXml(HttpServletRequest request) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(
 				(ServletInputStream) request.getInputStream()));
@@ -87,12 +87,12 @@ public class WxpayNotifyController {
 		Element root = document.getRootElement();
 		return root;
 	}
-	
+
 	@RequestMapping(value = "/notify", method = {RequestMethod.GET, RequestMethod.POST})
 	public String wxnotify(HttpServletRequest request,
 			HttpServletResponse response) throws Exception{
-		 WXPay wxPay = new WXPay(conf, notifyUrl);
-		 
+		WXPay wxPay = new WXPay(conf, notifyUrl);
+
 		Element root = this.getRootElement(request);
 		String returnCode = root.element("return_code").getText();
 		Element e = root.element("return_msg");
@@ -119,7 +119,9 @@ public class WxpayNotifyController {
 				Map<String, String> reqData = WXPayUtil.xmlToMap(this.getXml(request));
 				boolean resultSignValid = wxPay.isPayResultNotifySignatureValid(reqData);
 				//如果验证通过并且订单支付金额与支付结果通知返回金额相同则进行投保
+				logger.info("orderAmount:"+payMessage.getOrderAmount()*100+"total_fee:"+wxPayResult.getTotal_fee()+resultSignValid);
 				if(Integer.toString((payMessage.getOrderAmount()*100)).equals(wxPayResult.getTotal_fee())&&resultSignValid) {
+					logger.info("验证成功");
 					//如果订单状态为已经支付，则直接返回成功，不继续进行投保
 					if(payMessage.getPayStatus().equals(BaseResponse.pay_staus)) {
 						return document.asXML();
@@ -132,15 +134,15 @@ public class WxpayNotifyController {
 				e1.printStackTrace();
 			}
 			//发送成功短信
-		    smsStatus = smsCodeService.sendSms(payMessage.getInsuranttel(), "伟林易航",templateCode,"");
-		    logger.info("订单号为:"+payMessage.getOrderNo()+";手机号为："+payMessage.getInsuranttel()+"的订单成功短信通知" + smsStatus);
+			smsStatus = smsCodeService.sendSms(payMessage.getInsuranttel(), "伟林易航",templateCode,"");
+			logger.info("订单号为:"+payMessage.getOrderNo()+";手机号为："+payMessage.getInsuranttel()+"的订单成功短信通知" + smsStatus);
 		} else {
 			//发送支付失败短信
 			logger.info("订单支付失败");
 		}
 		return document.asXML();
 	}
-	
+
 	/**
 	 * 微信支付平台返回xml数据转换为对象
 	 * @param xmlStr
@@ -187,5 +189,5 @@ public class WxpayNotifyController {
 		wxPay.setTransaction_id(transaction_id);
 		return wxPay;
 	}
-	
+
 }
